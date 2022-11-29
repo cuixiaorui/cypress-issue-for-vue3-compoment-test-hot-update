44fbe65da72f508af8da7669b9244f883057dc12
cuixiaorui <cui_xiaorui@126.com>
2022-11-29 21:22:51 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1 className="text-3xl font-bold underline bg-red-500">Hello world!</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+
+</script>
+<style>
+
+</style>
